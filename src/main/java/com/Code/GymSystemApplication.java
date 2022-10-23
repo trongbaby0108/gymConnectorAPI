20abafaca9c38ef9806f4184f7f16cd8e3488a01
DESKTOP-R98UOPN\trong <tronghandsome1111@gmail.com>
2022-10-23 21:24:07 +0700

    update

--- src/main/java/com/Code/GymSystemApplication.java
+++ src/main/java/com/Code/GymSystemApplication.java
@@ -6,10 +6,10 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class DoanCosoBackendApplication {
+public class GymSystemApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DoanCosoBackendApplication.class, args);
+		SpringApplication.run(GymSystemApplication.class, args);
 	}
 
 }
